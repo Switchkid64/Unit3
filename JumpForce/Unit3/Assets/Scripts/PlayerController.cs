7234baade28509a5d7fb121915d173138e199943
Switchkid64 <minaj@roverkids.org>
2025-06-03 13:50:50 -0400

    this is all I could get done

--- JumpForce/Unit3/Assets/Scripts/PlayerController.cs
+++ JumpForce/Unit3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,7 +27,13 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter (Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("ground")) {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("obstacle")) {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 
 }
